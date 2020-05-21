259c31d52adf2080d6bf14a27f8f0c4475560c81
Vivek Kosare <v_kosare@yahoo.com>
2020-05-21 10:59:36 +0200

    implemented error component for exception handling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -24,15 +24,19 @@ const person = (props) =>{
     //         width:'450px'
     //     }
     // }
-return (
-     <div className={classes.Person}>
-  
-        <p onClick={props.click}>I am a {props.name} and I am {props.age} years old</p>
-        <p>{props.children}</p>
-        <input type="text" onChange={props.changed} value={props.name} />
+    const rand = Math.random();
+    if(rand > 0.7){
+        throw new Error('some thing happened')
+    }
+    return (
+        <div className={classes.Person}>
     
-     </div>
-    )
+            <p onClick={props.click}>I am a {props.name} and I am {props.age} years old</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        
+        </div>
+        )
 }
 
 // export default Radium(person);
